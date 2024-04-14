a3bd15c8bc2c159aebe28208526df9dfc7d43dc6
Vic liu <yuqi_jiu@163.com>
2024-04-14 17:31:33 +0800

    '这是一次静态文件的提交'

--- Blog_project/settings.py
+++ Blog_project/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True #
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -161,3 +164,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 # # 使用的缓存别名（默认内存缓存）
 # SESSION_CACHE_ALIAS = "default"
+
